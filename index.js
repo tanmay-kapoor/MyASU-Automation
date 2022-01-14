eaddf51694beb5238c85ae7dc2cc0715f38c1159
Tanmay Kapoor <tanmay.skater@gmail.com>
2022-01-14 21:25:16 +0530

    changes for heroku

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const webdriver = require("selenium-webdriver");
-const cmd = require("node-cmd");
-
 const automate = async () => {
-    const driver = new webdriver.Builder().forBrowser("chrome").build();
+    const cmd = require("node-cmd");
+    const webdriver = require("selenium-webdriver");
+    require("chromedriver");
+    const chrome = require("selenium-webdriver/chrome");
+
+    const options = new chrome.Options();
+    options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+    const serviceBuilder = new chrome.ServiceBuilder(
+        process.env.CHROME_DRIVER_PATH
+    );
+
+    const screen = {
+        width: 1920,
+        height: 1080,
+    };
+
+    // Essential for heroku
+    options.addArguments("--headless");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");
+    options.windowSize(screen);
+
+    const driver = new webdriver.Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(ooptions)
+        .setChromeService(serviceBuilder)
+        .build();
 
     await driver.get("https://webapp4.asu.edu/myasu/");
     driver.findElement(webdriver.By.id("username")).sendKeys("tkapoor4");
@@ -21,7 +44,7 @@ const automate = async () => {
     driver.quit();
 
     cmd.runSync(`node sendMail.js ${text}`);
-    setTimeout(await automate, 1800000);
+    setTimeout(await automate, 5000);
 };
 
 const foo = async () => {
